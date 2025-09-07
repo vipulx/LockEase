b019af47657086853cfc8e5b774e6ba111564430
Vipul Raj <155657964+vipulx@users.noreply.github.com>
2025-09-08 02:54:34 +0530

    Update TempData.h

--- TempData.h
+++ TempData.h
@@ -23,13 +23,14 @@ ota:
 wifi:
   ssid: !secret wifi_ssid
   password: !secret wifi_password
-
-  # Enable fallback hotspot (captive portal) in case wifi connection fails
   ap:
     ssid: "Doordash Fallback Hotspot"
     password: "Xl8dbl2PNe66"
 captive_portal:
-# --- Security codes ---
+
+# ------------------------
+# Globals
+# ------------------------
 globals:
   - id: disarm_code
     type: std::string
@@ -37,8 +38,42 @@ globals:
   - id: arm_code
     type: std::string
     initial_value: '"9999"'
+  - id: show_welcome
+    type: bool
+    initial_value: 'true'
 
-# --- Alarm control state ---
+# ------------------------
+# Time
+# ------------------------
+time:
+  - platform: homeassistant
+    id: home_time
+
+# ------------------------
+# Interval to remove welcome screen after 2s
+# ------------------------
+interval:
+  - interval: 5s
+    then:
+      - lambda: |-
+          id(show_welcome) = false;
+
+# ------------------------
+# Lights
+# ------------------------
+light:
+  - platform: esp32_rmt_led_strip
+    rgb_order: GRB
+    chipset: WS2812
+    pin: GPIO16
+    num_leds: 3
+    name: "Status Light"
+    id: status_light
+    restore_mode: ALWAYS_OFF
+
+# ------------------------
+# Text Sensors
+# ------------------------
 text_sensor:
   - platform: template
     id: alarm_control
@@ -48,7 +83,23 @@ text_sensor:
     id: entered_pin
     name: "Entered PIN"
 
-# --- Keypad Matrix (4x4) ---
+  - platform: template
+    id: last_rfid
+    name: "Last RFID Tag"
+
+  - platform: homeassistant
+    id: alarm_status
+    entity_id: alarm_control_panel.your_alarm_entity_id
+    internal: true
+
+  - platform: wifi_info
+    ip_address:
+      name: "LockEase IP"
+      id: my_ip
+
+# ------------------------
+# Keypad Matrix (4x4)
+# ------------------------
 matrix_keypad:
   id: my_keypad
   rows:
@@ -61,11 +112,9 @@ matrix_keypad:
     - pin: GPIO7
     - pin: GPIO5
     - pin: GPIO3
-
   keys: "123A456B789C*0#D"
   has_diodes: true
 
-# Key Collector
 key_collector:
   - id: pincode_reader
     source_id: my_keypad
@@ -76,26 +125,79 @@ key_collector:
     clear_keys: "*"
     allowed_keys: "0123456789"
     timeout: 5s
-    on_progress:
-      - logger.log:
-          format: "input progress: '%s', started by '%c'"
-          args: [ 'x.c_str()', "(start == 0 ? '~' : start)" ]
     on_result:
-      - logger.log:
-          format: "input result: '%s', started by '%c', ended by '%c'"
-          args: [ 'x.c_str()', "(start == 0 ? '~' : start)", "(end == 0 ? '~' : end)" ]
-    on_timeout:
-      - logger.log:
-          format: "input timeout: '%s', started by '%c'"
-          args: [ 'x.c_str()', "(start == 0 ? '~' : start)" ]
+      - text_sensor.template.publish:
+          id: entered_pin
+          state: !lambda 'return x;'
+      - if:
+          condition:
+            lambda: 'return x == id(disarm_code);'
+          then:
+            - text_sensor.template.publish:
+                id: alarm_control
+                state: "Disarmed"
+            - logger.log: "System Disarmed by PIN"
+      - if:
+          condition:
+            lambda: 'return x == id(arm_code);'
+          then:
+            - text_sensor.template.publish:
+                id: alarm_control
+                state: "Armed"
+            - logger.log: "System Armed by PIN"
+      - if:
+          condition:
+            lambda: 'return x != id(disarm_code) && x != id(arm_code);'
+          then:
+            - logger.log: "Invalid PIN Entered"
 
- # Status Light
-# light:
-#   - platform: esp32_rmt_led_strip
-#     rgb_order: GRB
-#     chipset: WS2812
-#     pin: GPIO15
-#     num_leds: 1
-#     name: "Status Light"
-#     id: status_light
-#     restore_mode: ALWAYS_OFF
+# ------------------------
+# OLED Display (SSD1306 0.96")
+# ------------------------
+i2c:
+  sda: GPIO17
+  scl: GPIO18
+  scan: true
+  
+display:
+  - platform: ssd1306_i2c
+    model: "SSD1306 128x64"
+    address: 0x3C
+    id: oled_display
+    lambda: |-
+      if (id(show_welcome)) {
+        // Show centered large "EaseLock" welcome message
+        it.printf(0, 16, id(fontd), TextAlign::CENTER, "EaseLock");
+      } else {
+        // Show time centered at top
+        it.strftime(0, 0, id(fontd), TextAlign::CENTER, "%H:%M:%S", id(home_time).now());
+        // Show Alarm status below time, left aligned
+        it.printf(0, 48, id(font2), "Alarm: %s", id(alarm_status).state.c_str());
+      }
+
+
+# ------------------------
+# Fonts
+# ------------------------
+font:
+  - file: "ARIAL.TTF"
+    id: font1
+    size: 12
+
+  - file: "ARIAL.TTF"
+    id: font2
+    size: 20
+
+  - file: "ARIAL.TTF"
+    id: font3
+    size: 43
+
+  - file: "fonts/digital-7m.ttf"
+    id: fontd
+    size: 43
+
+# Restart switch to allow restarting from Home Assistant
+switch:
+  - platform: restart
+    name: "EaseLock ESP Restart"
+    id: esp_restart
